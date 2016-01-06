94454f30648b4bd613cc1a220ed31b1672b793ce
Peter Fonseca <peter.fonseca@ucsf.edu>
2016-01-06 15:48:02 -0800

    adding rvs_vis.py to repo

---
+++ RVS_vis.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import pandas as pd
+# from ggplot import *
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+
+# import and massage data
+df = pd.read_csv("RVS_report.csv", header=None, names=["att", "date", "num_rvs", "num_old"])
+df['num_new'] = df['num_rvs'] - df['num_old']
+
+cols_to_keep = ['att', 'num_new', 'num_old']
+df_vis = df[cols_to_keep].tail(12)
+
+# df_long = pd.melt(df.tail(12), id_vars=['att'], value_vars=['num_new', 'num_old'])
+
+#ggplot(df_long, aes(x='att', y='value', fill='variable')) +\
+#             geom_bar(stat='identity') +\
+#             scale_fill_manual(values=c["#404040, #9e2929"])
+
+# initialize vars for categorical axis
+att_call = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+att_labels = df_vis.att
+
+
+df_vis.plot(kind='bar', stacked=True, color=['#666666', '#C94949'])
+plt.xticks(att_call, att_labels, rotation=45)
+plt.xlabel("Attending")
+plt.ylabel("Count")
+plt.title("Outstanding RVSs")
+
+plt.show()
