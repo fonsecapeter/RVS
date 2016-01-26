a4194242339d9de5990c59173b0b789015bde44d
Peter Fonseca <peter.fonseca@ucsf.edu>
2016-01-26 09:54:45 -0800

    restructure menus, adddition of utils

--- jarvs.sh
+++ jarvs.sh
@@ -6,200 +6,284 @@
 #  * set arguments for other useful commands
 #  * maintain list of <command> s
 
-tput setaf 2
-	echo "Hello, ${USER}. What can I help you with?"
-tput sgr0
+set_color () {
+	# introduce jarvs if first time using
+	if [ ! -f ./clr.txt ]; then
+		echo "Hello, ${USER}"
+		echo "My name is Jarvs, and I am your personal assistant."
+		echo "Peter built me to help manage the rvs program."
+		echo "I hope you find that I make life easier."
+		echo "Before we get started, what is your favorite color?"
+	else
+		echo "What is your favorite color?"
+	fi
+	echo "choices are: red, green, orange,"
+	echo "blue, purple, light-blue, and white"
+	read fav_clr
 
-# main menu ----------------------------------------
+	case "$fav_clr" in
+		red)
+			echo "1" > ./clr.txt
+		;;
 
+		green)
+			echo "2" > ./clr.txt
+		;;
+
+		orange)
+			echo "3" > ./clr.txt
+		;;
+
+		blue)
+			echo "4" > ./clr.txt
+		;;
+
+		purple)
+			echo "5" > ./clr.txt
+		;;
+
+		light-blue)
+			echo "6" > ./clr.txt
+		;;
+
+		white)
+			echo "7" > ./clr.txt
+		;;
+
+		*)
+			echo "I'm sorry, I don't know that one."
+			echo "Try typing your color in lower-case."
+		;;
+	esac
+
+	clr=`cat ./clr.txt`
+	tput setaf $clr
+		echo "Much better, Thanks!"
+		echo "If you ever change your mind,"
+		echo "just ask me to change your preferences."
+	tput sgr0
+}
+
+# if first time using jarvs, set favorite color
+if [ ! -f ./clr.txt ]; then
+	set_color
+fi
+
+clr=`cat ./clr.txt`
+
+puts () {
+	output=$1
+	tput setaf $clr
+		echo "$1"
+	tput sgr0
+}
+
+puts "Hello, ${USER}. What can I help you with?"
+
+# put all menu's in infinite loop ----------------------
 while true; do
-	menu="main"
 
+	menu="main"
+	# main menu ----------------------------------------
 	while [ $menu == "main" ]; do
 		read cmd
 
 		case "$cmd" in
 
-		*"bye"*)
-			tput setaf 2
-				echo "Goodbye, let me know if you need anything else."
-			tput sgr0
-			exit
-		;;
-
 		*"list"*)
-			tput setaf 2
-				echo "<bye> <help> <list> <eamil> <report>"
-			tput sgr0
+			puts "<bye> <help> <list> <preferences> <eamil> <report>"
 			continue
 		;;
 
-		*"email"*)
-			tput setaf 2
-				echo "Of course, I am programmed to help you send emails."
-			tput sgr0
-			menu="email"
-			break
-		;;
-
 		*"report"*)
-			tput setaf 2
-				echo "Of course, I am programmed to tell you how the attendings are doing."
-			tput sgr0
+			puts "Of course, I am programmed to tell you how the attendings are doing."
 			menu="report"
 			break
 		;;
 
+		*"email"*)
+			puts "Of course, I am programmed to help you send emails."
+			menu="email"
+			break
+		;;
+
 		*"help"*)
-			tput setaf 2
-				echo "I am your personal assistant, you can call me Jarvs."
-				echo "Peter built me to help you manage the rvs program."
-				echo "All you have to do is give me a command."
-				echo "Ask me for a list to see what commands I am programmed"
-				echo "to understand"
-			tput sgr0
-			continue
-		;;
-
-		*"thank"*)
-			tput setaf 2
-				echo "No need to thank me, it's my job."
-			tput sgr0
-			continue
-		;;
-
-		*"how are you"*)
-			tput setaf 2
-				echo "I can't complain"
-			tput sgr0
+			puts "I am your personal assistant, you can call me Jarvs."
+			puts "Peter built me to help you manage the rvs program."
+			puts "All you have to do is give me a command."
+			puts "Ask me for a list to see what commands I am programmed"
+			puts "to understand"
+			puts "You can always tell me to take a break by saying bye."
 			continue
 		;;
 
 		*)
-			tput setaf 2
-				echo "I'm terribly sorry, I don't understand that."
-				echo "Try typing 'help' or 'bye'"
-			tput sgr0
-			continue
+			prev_menu="$menu"
+			menu="utils"
+			break
 		;;
+
 		esac
 	done
 
-# reporting menu ----------------------------------------
-
-	while [ $menu == "report" ]; do
-		#if [menu != "report"]; then
-		#	break
-		#fi
-		tput setaf 2
-			echo "How can I help you with reporting?"
-		tput sgr0
-		read cmd
-
+	# utilities menu ----------------------------------------	
+	while [ $menu == "utils" ]; do
 		case $cmd in
-
-		*"done"*)
-			tput setaf 2
-				echo "Fantastic, what else can I do for you?"
-				menu="main"
-			tput sgr0
-			continue 2
-		;;
-
-		*"bye"*)
-			tput setaf 2
-				echo "Goodbye, let me know if you need anything else."
-			tput sgr0
-			exit
-		;;
-
-		*"report"*)
-			tput setaf 2
-				echo "No problem, let me crunch the numbers."
-				echo "I'll show you all the rvs's currently waiting"
-				echo "for approval"
-				./RVS_reporter.sh
-			tput sgr0
-			continue
-		;;
-
-		*"vis"*)
-			tput setaf 2
-				echo "No problem, let me crunch the numbers."
-				echo "I'll show you all the rvs's waiting for approval"
-				echo "since the last time I reported."
-				echo "I won't log the data on this one"
-				./RVS_vis.py
-			tput sgr0
-			continue
-		;;
-
-		*help*)
-			tput setaf 2
-				echo "You last told me that wanted some help reporting."
-				echo "If you need something else, just let me know."
-			tput sgr0
-			continue
-		;;
-
-		*)
-			tput setaf 2
-				echo "I'm terribly sorry, I don't understand that."
-				echo "Try typing 'help' or 'bye'"
-			tput sgr0
-			continue
-		;;
-		esac
-	done
-
-# emailing menu ----------------------------------------
-	while [ menu == "email" ]; do
-		tput setaf 2
-			echo "How can I help you with emails?"
-		tput sgr0
-		read cmd
-
-		case $cmd in
-
-			*"done"*)
-				tput setaf 2
-					echo "Fantastic, what else can I do for you?"
-					menu="main"
-				tput sgr0
-				continue 2
-			;;
 
 			*"bye"*)
-				tput setaf 2
-					echo "Goodbye, let me know if you need anything else."
-				tput sgr0
+				puts "Goodbye, let me know if you need anything else."
 				exit
 			;;
 
-			*"email"*)
-				tput setaf 2
-					echo "No problem, let me write these up."
-					./RVS_emailer.sh
-				tput sgr0
-				continue
+			*"done"*|*"nothing"*|*"main"*)
+				puts "Fantastic, what else can I do for you?"
+				menu="main"
+				break
 			;;
 
-			*help*)
-				tput setaf 2
-					echo "You last told me that wanted some help sending emails."
-					echo "If you need something else, just let me know."
-				tput sgr0
+			*"date"*)
+				puts "Always good to stay oriented."
+				echo ""
+				cal
+				echo ""
+			;;
+
+			# need to download icalbuddy and test on mac
+			*"today"*)
+				puts "Here is your agenda for the day:"
+				echo ""
+				icalbuddy -f -sc eventsToday
+				echo ""
+			;;
+
+			*"preference"*)
+				puts "I do appreciate feedback."
+				menu="preferences"
+				break
+			;;
+
+			*"report"*)
+				puts "Of course, I am programmed to tell you how the attendings are doing."
+				menu="report"
+				break
+			;;
+
+			*"email"*)
+				puts "Of course, I am programmed to help you send emails."
+				menu="email"
+				break
+			;;				
+
+			*"thank"*)
+				puts "No need to thank me, it's my job."
+			;;
+
+			*"how are you"*)
+				puts "I can't complain"
 				continue
 			;;
 
 			*)
-				tput setaf 2
-					echo "I'm terribly sorry, I don't understand that."
-					echo "Try typing 'help' or 'bye'"
-				tput sgr0
+				puts "I'm terribly sorry, I didn't understand that."
+				puts "Try typing help, list, done, or bye."
+				puts "Make sure you are typing in lower-case."
+			;;
+
+		esac
+		menu="$prev_menu"
+		break
+	done
+
+	# preferences menu ----------------------------------------
+	while [ $menu == "preferences" ]; do
+		puts "What can I do differently?"
+		read cmd
+
+		case $cmd in
+
+			*"color"*)
+				set_color
 				continue
 			;;
-			esac
-		done
+
+			*help*)
+				puts "You last told me that wanted to edit your preferences."
+				puts "If you need something else, just let me know."
+				continue
+			;;
+
+			*)
+				prev_menu="$menu"
+				menu="utils"
+				break
+			;;
+		esac
+	done
+
+	# reporting menu ----------------------------------------
+	while [ $menu == "report" ]; do
+
+		puts "How can I help you with reporting?"
+		read cmd
+
+		case $cmd in
+
+			*"report"*)
+				puts "No problem, let me crunch the numbers."
+				puts "I'll show you all the rvs's currently waiting"
+				puts "for approval"
+				./RVS_reporter.sh
+				continue
+			;;
+
+			*"vis"*)
+				puts "No problem, let me crunch the numbers."
+				puts "I'll show you all the rvs's waiting for approval"
+				puts "since the last time I reported."
+				puts "I won't log the data on this one"
+				./RVS_vis.py
+				continue
+			;;
+
+			*help*)
+				puts "You last told me that wanted some help reporting."
+				puts "If you need something else, just let me know."
+				continue
+			;;
+
+			*)
+				prev_menu="$menu"
+				menu="utils"
+				break
+			;;
+		esac
+	done
+
+	# emailing menu ----------------------------------------
+	while [ $menu == "email" ]; do
+		puts "How can I help you with emails?"
+		read cmd
+
+		case $cmd in
+
+			*"email"*)
+				puts "No problem, let me write these up."
+				./RVS_emailer.sh
+				continue
+			;;
+
+			*help*)
+				puts "You last told me that wanted some help sending emails."
+				puts "If you need something else, just let me know."
+				continue
+			;;
+
+			*)
+				prev_menu="$menu"
+				menu="utils"
+				break
+			;;
+		esac
+	done
 
 done
